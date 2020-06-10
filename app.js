c46ef292b80f24d2e038650d20a5dc5203ced71b
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-06-10 14:17:15 +0200

    add fonction randomColors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 
 /* Fonctions */
+/* Fonction permettant de générer le code Hexadecimal d'une couleur */
 function generateHex() {
   const letters = "0123456789ABCDEF";
   let hash = "#";
@@ -14,5 +15,18 @@ function generateHex() {
   return hash;
 }
 
+/* fonction permettant l'affichage d'une couleur ayant été généré par la fonction generateHex */
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    /* add couleur au background */
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+
 let randomHex = generateHex();
+randomColors();
 console.log(randomHex);
